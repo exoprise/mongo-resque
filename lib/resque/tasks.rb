c08fb900437424c2c42485344231da6d91942aff
bughit <bughit@users.noreply.github.com>
2017-02-22 16:28:02 -0500

    support module reloading for resque:work in dev mode

--- lib/resque/tasks.rb
+++ lib/resque/tasks.rb
@@ -51,7 +51,7 @@ namespace :resque do
   task :preload => :setup do
     if defined?(Rails) && Rails.respond_to?(:application)
       # Rails 3
-      Rails.application.eager_load!
+      Rails.application.eager_load! unless Rails.env.development?
     elsif defined?(Rails::Initializer)
       # Rails 2.3
       $rails_rake_task = false
